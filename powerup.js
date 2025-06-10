9a9d08885de55ac0c63a3387446f8c8de606dfec
paulLTD <paul.n@ltdarch.co.nz>
2025-06-10 14:23:01 +1200

    Add files via upload

--- powerup.js
+++ powerup.js
@@ -6,18 +6,16 @@ var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4
 var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
 
 window.TrelloPowerUp.initialize({
-  'board-buttons': function(t, opts) {
+  'board-buttons': function(t, options) {
     return [{
-      icon: {
-        dark: WHITE_ICON,
-        light: BLACK_ICON
-      },
-      text: 'Generate Card Table',
+      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+      text: 'Generate Report',
       callback: function(t) {
         return t.popup({
-          title: 'Card Names Table',
+          title: 'Job Report',
           url: './table.html',
-          height: 400
+          height: 600
+          width: 600
         });
       },
       condition: 'always'
