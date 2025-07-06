b74d7c844524d243abf5984fd45367b030222690
paulLTD <paul.n@ltdarch.co.nz>
2025-07-07 11:36:06 +1200

    Add files via upload

---
+++ client.js
@@ -0,0 +1,24 @@
+// JavaScript source code
+
+var WHITE_ICON = 'https://paulltd.github.io/LTD/white_icon.png';
+var BLACK_ICON = 'https://paulltd.github.io/LTD/black_icon.png';
+var onBtnClick = function (t, opts) {console.log('Someone clicked the button');
+
+
+TrelloPowerUp.initialize({
+    'board-buttons': function (t, opts) {
+        return [
+
+            {
+                "name": "Recent Card Activity",
+                "description": "Displays a view of recent activity for each card on the board.",
+                "icon": {
+                    dark: WHITE_ICON,
+                    light: DARK_ICON
+                },
+                "text": "My Board Button",
+                "callback": "function(t)",
+        { "console.log('clicked');" }
+      }
+    ]
+})
