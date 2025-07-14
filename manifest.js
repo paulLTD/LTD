dc7fa696ac5c4722c228af2fff29848ee4f27d77
paulLTD <paul.n@ltdarch.co.nz>
2025-07-14 12:38:07 +1200

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,6 @@
+{
+    "name": "LTD Report",
+        "connectorUrl": "https://paulltd.github.io/LTD/index.html",
+            "capabilities": ["card-buttons", "board-buttons"],
+        "icon": "https://paulltd.github.io/LTD/white_icon.png"
+}
