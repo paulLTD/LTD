2dba525e47b85b9a042313618b1f499436c48541
paulLTD <paul.n@ltdarch.co.nz>
2025-07-14 12:48:34 +1200

    Add files via upload

--- client.js
+++ client.js
@@ -1,9 +1,17 @@
-// JavaScript source code
-
-TrelloPowerUp.initialize({
+/* global TrelloPowerUp */
+var t = TrelloPowerUp.initialize({
     'board-buttons': function (t, options) {
         return [{
-                    icon: 'https://paulltd.github.io/LTD/white_icon.png',
-                    text: "My Board Button",
-               }]
-})
+            icon: 'https://paulltd.github.io/LTD/icon.png', // Optional: Add an icon file at this path if desired
+            text: 'LTD Button',
+            callback: function (t) {
+                // No action performed for now
+                return t.popup({
+                    title: 'LTD Report',
+                    url: './index.html',
+                    height: 200
+                });
+            }
+        }];
+    }
+});
