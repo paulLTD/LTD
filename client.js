083f787fff26cd000637c438300c26088d203e9c
paulLTD <paul.n@ltdarch.co.nz>
2025-07-15 15:04:09 +1200

    Add files via upload

--- client.js
+++ client.js
@@ -8,7 +8,7 @@ var t = TrelloPowerUp.initialize({
                 // No action performed for now
                 return t.popup({
                     title: 'LTD Report',
-                    url: './index.html',
+                    url: './list-cards.html',
                     //args: { cards: cards },
                     height: 400
                  });
